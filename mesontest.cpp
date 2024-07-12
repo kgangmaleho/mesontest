ea1f2aa0509d4107bfcf60200a60cc61bc53a163
kgang maleho <kgang.maleho@wirk.co.za>
2024-07-12 10:06:55 +0200

    Added new library and test

--- mesontest.cpp
+++ mesontest.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
 int main() {
     std::cout << "New Repository!" << "\n";
     std::cout << "Testing Changes!" << "\n";
+
+    SDL_Renderer *renderer;
+    SDL_Window *window = SDL_CreateWindow("SDL2 Meson Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    while(1) {
+        
+    }
     return 0;
 }
